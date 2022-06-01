3b360a68abf6938dee9f35affc6ee80a03ef3d22
mosman <defmark897@protonmail.com>
2022-06-01 22:13:50 +0900

    all most finished js

---
+++ script.js
@@ -0,0 +1,82 @@
+//コンピュータの手を選ぶ
+
+function computerPlay() {
+    //０から２までのランダムな数を作る
+    let num = Math.floor(Math.random()*3);
+    //じゃんけんの手の入った配列を作る
+    const hands = ["rock", "paper", "scissors"];
+    //配列から指定された位置の手を返す
+    return hands[num];
+}
+
+//１ラウンドの勝敗を決める
+
+function playRound(playerSelection, computerSelection) {
+    //プレイヤーの手を小文字にする
+    playerSelection = playerSelection.toLowerCase()
+
+    //プレイヤーの手とコンピュータの手を比べる
+    if (playerSelection == computerSelection) {
+        return "draw";
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "scissors") {
+            return 'You win! Rock beats scissors';
+        } else {
+            return "you lose...paper beats Rock";
+        }
+     } else if (playerSelection = "scissors") {
+        if (computerSelection == "paper") {
+            return "You win! Scissors beats Paper";
+        } else {
+            return "You lose...Rock beats Scissors";
+        }
+    } else {
+        if (computerSelection == "rock") {
+            return "You win! Paper beats Rock";
+        } else {
+            return "You lose...Scissors beats Paper";
+        }
+    }
+}
+
+function game() {
+    //プレイヤーとコンピュータの勝利数を初期化
+    let computerWin = 0;
+    let playerWin = 0;
+
+    for (let i = 0; i < 5; i++){
+        //プレイヤーの手を取得
+        let playerSelection = prompt("Please input your next move");
+        // 正しくない場合何度も問い直す
+
+        //その試合のメッセージを取得し、表示
+        let result = playRound
+        (playerSelection, computerPlay());
+        
+        console.log(result);
+
+        //　メッセージの中にwinの単語があれば勝利とみなし、プレイヤーの勝利数に加算し、loseの文字があればコンピュータ側に加算
+        if (result.includes("win")) {
+            playerWin += 1;
+        } else if (result.includes("lose")) {
+            computerWin += 1;
+        }
+    }
+    // 勝ち数の表示
+    function printTotalScore () {
+        console.log(`YOU: ${playerWin} COM: ${computerWin} DRAW: ${5 - computerWin - playerWin}`);
+    }
+    // 勝敗を表示
+    if (playerWin > computerWin) {
+            console.log("YOU WIN!");
+            printTotalScore();
+        } else if (playerWin == computerWin) {
+            console.log("DRAW");
+            printTotalScore();
+        } else {
+            console.log("YOU LOSE...");
+            printTotalScore();
+        }
+}
+
+game();
